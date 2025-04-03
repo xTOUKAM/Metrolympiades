c7047162c0b16080188d2c2d08ad80e492965c5a
xTOUKAM <houllegatte.tom@gmail.com>
2025-04-03 21:13:13 +0200

    Modification LoginPage

--- metrolympiades/src/pages/HomePage.vue
+++ metrolympiades/src/pages/HomePage.vue
@@ -1,7 +1,36 @@
 <script setup>
 import SideBar from '@/components/SideBar.vue';
+import { useAuth } from '@/utils';
+
+const { isLoggedIn, teamName, checkAuth } = useAuth();
+
+checkAuth(); // Vérification de connexion
+
+const navigateToLogin = () => {
+  const router = useRouter();
+  router.push("/login");
+};
 </script>
 
+
 <template>
+  <div class="home-page">
     <SideBar />
+    <main>
+      <h1 v-if="isLoggedIn">Bienvenue, {{ teamName }}</h1>
+      <h1 v-else>Bienvenue sur Metrolympiades</h1>
+      <p v-if="isLoggedIn" class="description">
+        Accédez au classement général, gérez votre équipe, et découvrez vos prochains matchs.
+      </p>
+      <p v-else class="description">
+        Découvrez les classements et connectez-vous pour accéder à toutes les fonctionnalités !
+      </p>
+      <button
+        v-if="!isLoggedIn"
+        class="cta-button"
+        @click="navigateToLogin">
+        Se connecter
+      </button>
+    </main>
+  </div>
 </template>
